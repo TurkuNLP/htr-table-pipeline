673f854c127e06efd4e8bf343897fedd24b1daae
Filip Ginter <filip.ginter@gmail.com>
2025-03-28 19:08:31 +0200

    gathering now both original and deskewed relative coords; also fixing basename

--- page-deskew/gather_mid_point_metadata_from_autods.py
+++ page-deskew/gather_mid_point_metadata_from_autods.py
@@ -3,6 +3,8 @@ import os
 import argparse
 import json
 from tqdm import tqdm
+import cv2
+import numpy as np
 
 class DeskewData:
     def __init__(self, content):
@@ -45,7 +47,7 @@ class DeskewData:
         self.bm_new=get_point(self.bm_old, bm_new)
         self.bl_new=get_point(self.bl_old, bl_new)
 
-    def get_mid_vertical_x(self):
+    def get_mid_vertical_x_deskewed(self):
         if self.tm_new is None or self.tl_new is None:
             return 0.0
         if self.tr_new is None or self.tm_new is None:
@@ -54,26 +56,57 @@ class DeskewData:
         right=(self.tr_new[0]-self.tm_new[0])
         return left/(left+right)
     
+    def get_mid_vertical_x_original(self, img_size):
+        img_width, img_height=img_size
+        if self.tm_new is None or self.tl_new is None:
+            return 0.0
+        if self.tr_new is None or self.tm_new is None:
+            return 1.0
+        mid=(self.tm_new[0]+self.bm_new[0])/2
+        return mid/img_width # relative to image width
+        
+    
     
 ## The format of the txt file is:
 # #DBG tl_old tl_new tm_old tm_new tr_old tr_new br_old br_new bm_old bm_new bl_old bl_new
 # DBG 62,75 64,89 1073,70 1063,93 2084,53 2063,41 71,1646 65,1658 1082,1641 1065,1650 2089,1632 2088,1616
 
+def clean_basename(name):
+    """Remove .txt/.jpg suffix and debug_/coords_ prefix from basename"""
+    basename=os.path.basename(name)
+    basename=os.path.splitext(basename)[0]  # remove extension
+    if basename.startswith('debug_'):
+        basename=basename[6:]
+    if basename.startswith('coords_'):
+        basename=basename[7:]
+    return basename
+
 def read_txt_from_zip(zip_path):
     with ZipFile(zip_path) as zf:
         for name in zf.namelist():
             if name.endswith('.txt'):
-                # get contents and decode assuming utf-8
                 content=zf.read(name).decode('utf-8')
-                basename=os.path.basename(name)
+                basename=clean_basename(name)
                 yield basename, content
 
+def read_image_sizes_from_zip(zip_path):
+    with ZipFile(zip_path) as zf:
+        for name in zf.namelist():
+            if name.endswith('.jpg'):
+                data=zf.read(name)
+                img=cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_UNCHANGED)
+                basename=clean_basename(name)
+                yield basename, (img.shape[1], img.shape[0])  # width, height
+
 def gather_mid_point_metadata_from_autods(zip_path):
+    image_sizes=dict(read_image_sizes_from_zip(zip_path))
     for basename, content in read_txt_from_zip(zip_path):
+        img_size=image_sizes[basename]
         deskew_data=DeskewData(content)
-        print(json.dumps({"basename":basename, "mid_vertical_x_point_relative":deskew_data.get_mid_vertical_x()}))
-
-
+        info_dict={"basename":basename,
+        "mid_vertical_x_point_deskewed_relative":deskew_data.get_mid_vertical_x_deskewed(),
+        "mid_vertical_x_point_original_relative":deskew_data.get_mid_vertical_x_original(img_size)}
+        print(json.dumps(info_dict), flush=True)
 
 
 if __name__=='__main__':
